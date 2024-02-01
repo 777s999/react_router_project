4d7af15b4895d3a08ff4837e0f5c3c84a0f1f2e9
Subrata Rout <subrataa916@gmail.com>
2024-02-01 16:12:34 +0530

    routing

--- reactRouter_App_Proj/src/App.jsx
+++ reactRouter_App_Proj/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./componets/Header/Header";
+import Home from "./componets/Home/Home";
+import Footer from "./componets/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1 className="bg-green-400 p-4">React Router</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
